51af07caffb9e09c22bfea21dca8ec42a5a86561
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-09-03 11:09:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+console.log("I am wating for siskya ")
+
+
+app.use(cors());
+app.use(express.json());
+
+// useage of route 
