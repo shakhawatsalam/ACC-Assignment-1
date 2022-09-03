66d67bbb77d3887b413e4500d409c16dedf3b6dd
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-09-03 13:24:28 +0600

    random and get all user is done

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-console.log("I am wating for siskya ")
+const port = 5000;
+const http = require('http'); 
+const user = require('./UserData.json')
+// console.log(user);
 
 
 app.use(cors());
 app.use(express.json());
+// console.log(user);
 
-// useage of route 
+app.get('/', (req, res) => {
+    // const User = JSON.parse(user);
+    res.json("hello world")
+});
+app.get('/user/all', (req, res) => {
+    // const User = JSON.parse(user);
+    res.json(user)
+});
+app.get('/user/random', (req, res) => {
+    const randomNumber = Math.floor(Math.random() * user.length)+1;
+    const randomUser = user.find(ran => ran.id == randomNumber);
+    console.log(randomUser)
+    res.json(randomUser)
+});
+// http.createServer((function (req, res) {
+//     res.send(200, { "Content-Type: 'application/json"})
+//     res.end();
+// })).listen(port)
+
+// useage of route
+
+
+app.listen(port, () => {
+    console.log("listening to", port);
+})
